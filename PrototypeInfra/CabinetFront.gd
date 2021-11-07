303bd13dc0b8c6e42a249fb74d4b2c68021b485c
kaelstrom <kaelstrom@gmail.com>
2021-11-06 22:48:05 -0500

    made linked button and debug label ports

--- PrototypeInfra/CabinetFront.gd
+++ PrototypeInfra/CabinetFront.gd
@@ -10,7 +10,9 @@ var wires: Array
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var port = get_node("The Fixer Scene/The Circuit Board/OutputToGame/OutputPortDebug")
+	var val = port.getValue()
+	if(val != null):
+		get_node("DebugLabel").text = str(val)
